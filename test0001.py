1dfe02355470427def69c80c9367a3239608f83e
dohyunkk <dhkim240101@gmail.com>
2026-09-11 13:57:07 +0900

    test

---
+++ test0001.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2
+c = a + b
+print(c)
+
+f = c * 3
+print(f)
+
+# Ctrl + S 저장
+# Ctrl + F5 실행
+import numpy as np
+import pandas as pd
+import time
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.metrics import accuracy_score
+
